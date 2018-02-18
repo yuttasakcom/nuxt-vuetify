b286336a7b0fa23a8fdfb71f2db2a2bc90eb7820
YoProgrammer <yuttasakcom@gmail.com>
2018-02-18 20:52:45 +0700

    vue init vuetifyjs/nuxt

---
+++ layouts/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+      <Toolbar />
+      <v-content>
+          <v-container>
+              <nuxt />
+          </v-container>
+      </v-content>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from '@/components/Header/Toolbar'
+export default {
+  components: {
+    Toolbar
+  }
+}
+</script>
---
+++ pages/labs.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout>
+    test
+  </v-layout>
+</template>
+
+
+<script>
+export default {
+  layout: 'test'
+}
+</script>
